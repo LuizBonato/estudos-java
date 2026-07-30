5d3a3d280e32581a903f94b766ca322949ead6a5
LuizBonato <luizeduardo.bonato@outlook.com>
2026-07-29 23:40:30 -0300

    Exercicio 3 aprendendo sobre arrays/Collections

--- src/Exercicio3/Main.java
+++ src/Exercicio3/Main.java
@@ -1,5 +1,14 @@
-package Exercicio3;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
-
+    public static void main(String[] args) {
+        List<FiguraGeometrica> formas = new ArrayList<>();
+        formas.add(new Quadrado(5.0));
+        formas.add(new Retangulo(5.0, 3.0));
+        formas.add(new Circulo(5.0));
+        for (FiguraGeometrica s :formas ) {
+            System.out.println(s.calcularArea());
+        }
+    }
 }
